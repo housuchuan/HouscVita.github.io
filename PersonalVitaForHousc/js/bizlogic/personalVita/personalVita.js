5b1cff34a1f75fa6f4157cbf9563519962a04ddd
housc <1162138903@qq.com>
2017-01-17 14:03:39 +0800

    给自己的个人在线简历添加了一些动画效果

---
+++ PersonalVitaForHousc/js/bizlogic/personalVita/personalVita.js
@@ -0,0 +1,17 @@
+/**
+	*作者：侯苏川(housc)
+	*时间：2016-01-16 16:34:48
+	*描述：  a vita for housc
+ */
+
+//第一模块代码编写(首页)
+(function($){
+	//首页背景自定义动画
+	$('.page1').animate({
+		backgroundSize: '100%'
+	},1500);
+	//个人信息头颜色渐变
+	$('.data-introduction').animate({
+		opacity: .8
+	},3500);
+})(jQuery);
